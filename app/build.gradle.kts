8f953f420c186eb9004ed002b1001991732f7ba4
User154lt <63184070+user154lt@users.noreply.github.com>
2025-02-10 23:43:42 +0000

    Initial migration to multiplatform

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,53 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    kotlin("multiplatform")
+    id("org.jetbrains.compose")
     id("org.jetbrains.kotlin.plugin.compose")
+}
+
+kotlin{
+    androidTarget{
+        compilations.all{
+            kotlinOptions{
+                jvmTarget = "17"
+            }
+        }
+    }
+    jvm("desktop")
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    applyDefaultHierarchyTemplate()
+
+    sourceSets{
+        val commonMain by getting{
+            dependencies{
+                implementation(compose.ui)
+                implementation(compose.material3)
+                implementation(project(":searchable-dropdown"))
+            }
+        }
+        val androidMain by getting {
+            dependencies{
+                implementation("androidx.core:core-ktx:1.15.0")
+                implementation("androidx.activity:activity-compose:1.9.3")
+            }
+        }
+        val androidInstrumentedTest by getting {
+            dependencies {
+                implementation("androidx.test.ext:junit:1.2.1")
+                implementation("androidx.test.espresso:espresso-core:3.6.1")
+                implementation("androidx.compose.ui:ui-test-junit4:1.7.6")
+            }
+        }
+
+        val androidUnitTest by getting {
+            dependencies {
+                implementation("junit:junit:4.13.2")
+            }
+        }
+    }
 }
 
 android {
@@ -34,9 +80,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
     buildFeatures {
         compose = true
     }
@@ -51,18 +94,6 @@ android {
 }
 
 dependencies {
-
-    implementation(project(":searchable-dropdown"))
-
-    implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.activity:activity-compose:1.9.3")
-    implementation("androidx.compose.ui:ui:1.7.6")
-    implementation("androidx.compose.material3:material3:1.3.1")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.7.6")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.7.6")
     debugImplementation("androidx.compose.ui:ui-tooling:1.7.6")
     debugImplementation("androidx.compose.ui:ui-test-manifest:1.7.6")
 }
